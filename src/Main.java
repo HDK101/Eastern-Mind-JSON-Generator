f993a2b27c3612ab5ba2061af0635b6feb0ecb6c
Lucas Hideki Yamaguti Maziero <lucashideki87@gmail.com>
2019-04-13 18:11:00 -0300

    Place extraction implemented

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
+import com.easternmind.jsonmaker.*;
 
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        JSONMaker jsonMaker = new JSONMaker();
+        jsonMaker.GetAllFilenames();
     }
 }
--- src/com/easternmind/jsonmaker/JSONMaker.java
+++ src/com/easternmind/jsonmaker/JSONMaker.java
@@ -1,25 +1,71 @@
 package com.easternmind.jsonmaker;
 
+import java.io.*;
 import java.util.ArrayList;
-import java.io.File;
+import java.util.List;
 
 public class JSONMaker {
-    public ArrayList<EasternSaveObject> easternSaveObjects;
+    public List<EasternSaveObject> easternSaveObjects;
 
-    public void GetAllFilenames(){
+    public void GetAllFilenames() {
         File folder = new File("saves");
         try {
             File[] allSaveFiles = folder.listFiles();
             for (File tempFile : allSaveFiles) {
-                System.out.println(tempFile);
+                System.out.println("Converting " + tempFile.getPath() + "...");
+                ConvertIntoObject(tempFile.getPath());
+                //System.out.println(tempFile);
             }
+        } catch (NullPointerException ex) {
+            System.out.println("Folder doesn't exist or it's empty");
         }
-        catch (Exception ex){
+    }
+
+    public EasternSaveObject ConvertIntoObject(String fileName) {
+        EasternSaveObject tempEasternSaveObject = new EasternSaveObject();
+        BufferedReader bufferedReader = null;
+        try {
+            //Initialize bufferedReader
+            bufferedReader = new BufferedReader(new FileReader(fileName));
+
+            //Read file
+            //System.out.println(bufferedReader.lines().count());
+            List<String> lineList = new ArrayList();
+            int charCount = 0;
+            String line;
+
+            while ((line = bufferedReader.readLine()) != null) {
+                lineList.add(line);
+            }
+
+
+            String[] lines = new String[lineList.size()];
+            lines = lineList.toArray(lines);
+
+            //extract current place
+            char[] charLinePlace = lines[2].toCharArray();
+            String reverseCurrentPlace = "",currentPlace = "";
+            for (int i = charLinePlace.length - 1; i > 0; i--) {
+                if (charLinePlace[i] != '\\') reverseCurrentPlace += charLinePlace[i];
+                else break;
+            }
+            for (int i = reverseCurrentPlace.length() - 1; i >= 0; i--) {
+                currentPlace += reverseCurrentPlace.charAt(i);
+            }
+
+            System.out.println(currentPlace);
+
+        } catch (FileNotFoundException ex) {
             System.out.println(ex.getMessage());
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        } catch (ArrayIndexOutOfBoundsException ex) {
+            System.out.println("Array out of index in line: " + ex.getCause());
         }
         finally {
 
         }
+        return tempEasternSaveObject;
     }
 }
 
